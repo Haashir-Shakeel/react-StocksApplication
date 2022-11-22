2c473d9a57b398c127e804e009efc165f3638310
Haashir-Shakeel <david10james99@gmail.com>
2022-11-22 13:52:54 +0500

    added axios and setting baseUrl, working

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import finnHub from '../apis/finnHub'
 
 export const StockList = () => {
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT" , "AMZ"])
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/quote?symbol=MSFT&token=cdgtt42ad3i2r375gtb0cdgtt42ad3i2r375gtbg")
+        console.log(response);
+      } catch (error) {
+
+      }
+    }
+    fetchData()
+  },[])
+
   return (
     <div>StockList</div>
   )
