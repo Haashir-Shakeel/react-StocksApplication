b037ba7972775e3e103c06c6bd798c30a7f6845c
Haashir-Shakeel <david10james99@gmail.com>
2022-11-22 14:11:12 +0500

    check for component mounted

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -4,9 +4,11 @@ import { useState } from 'react'
 import finnHub from '../apis/finnHub'
 
 export const StockList = () => {
+  const [stock, setStock] = useState()
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT" , "AMZ"])
 
   useEffect(()=>{
+    let isMounted = true
     const fetchData = async () => {
       try {
         const response = await finnHub.get("/quote", {
@@ -15,11 +17,15 @@ export const StockList = () => {
           }
         })
         console.log(response);
+        //we dont want to set value if component is unmounted
+        isMounted && setStock(response.data)
       } catch (error) {
 
       }
     }
     fetchData()
+    //this will run whenever component gets unmounted 
+    return () => (isMounted = false)
   },[])
 
   return (
