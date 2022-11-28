a89c75a96f13b8edf00211639b8e202cc40e8729
Haashir-Shakeel <david10james99@gmail.com>
2022-11-28 14:32:57 +0500

    fetching data in stockOverview

--- src/pages/StockDetailsPage.jsx
+++ src/pages/StockDetailsPage.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
-
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import finnHub from '../apis/finnHub'
 export const StockDetailsPage = () => {
+  const {symbol} = useParams()
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const date = new Date()
+      const currentTime = Math.floor(date.getTime()/1000)
+      
+      let oneDay;
+      if (date.getDay() === 1){
+        oneDay = currentTime - 3 * 24 * 60 * 60
+      }else if(date.getDay() === 2){
+        oneDay = currentTime - 2 * 24 * 60 * 60
+      }else{
+        oneDay = currentTime - 24*60*60
+      }
+
+      const oneWeak = currentTime - 7 * 24 * 60 * 60
+      const oneYear = currentTime - 365 * 24 * 60 * 60
+
+      const response = await finnHub.get('/stock/candle',
+      {params:{
+        symbol: symbol,
+        from : oneWeak,
+        to : currentTime,
+        resolution: 60,
+      }
+    })
+
+    console.log(response);
+    }
+
+    fetchData()
+  },[])
   return (
-    <div>StockDetailsPage</div>
+    <div>StockDetailsPage {symbol}</div>
   )
 }
