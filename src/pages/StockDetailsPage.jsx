9d092482799e18c24ef43ef1fc33a6b61598af3c
Haashir-Shakeel <david10james99@gmail.com>
2022-11-29 12:10:57 +0500

    some reformatting, getting weekly, yearly and daily data

--- src/pages/StockDetailsPage.jsx
+++ src/pages/StockDetailsPage.jsx
@@ -6,31 +6,51 @@ export const StockDetailsPage = () => {
   const {symbol} = useParams()
   useEffect(()=>{
     const fetchData = async () =>{
+
       const date = new Date()
       const currentTime = Math.floor(date.getTime()/1000)
-      
-      let oneDay;
-      if (date.getDay() === 1){
-        oneDay = currentTime - 3 * 24 * 60 * 60
-      }else if(date.getDay() === 2){
-        oneDay = currentTime - 2 * 24 * 60 * 60
-      }else{
-        oneDay = currentTime - 24*60*60
-      }
+
+      //depends on if stock market was closed day before
+      const oneDay = currentTime - 24*60*60
 
       const oneWeak = currentTime - 7 * 24 * 60 * 60
       const oneYear = currentTime - 365 * 24 * 60 * 60
 
-      const response = await finnHub.get('/stock/candle',
-      {params:{
-        symbol: symbol,
-        from : oneWeak,
-        to : currentTime,
-        resolution: 60,
-      }
-    })
+      try{
+        const responses = await Promise.all([
+          finnHub.get('/stock/candle',
+          {params:{
+            symbol: symbol,
+            from : oneDay,
+            to : currentTime,
+            resolution: 30,
+          }
+        }),
+          finnHub.get('/stock/candle',
+          {params:{
+            symbol: symbol,
+            from : oneWeak,
+            to : currentTime,
+            resolution: 60,
+          }
+        }),
+          finnHub.get('/stock/candle',
+          {params:{
+            symbol: symbol,
+            from : oneYear,
+            to : currentTime,
+            resolution: "W",
+          }
+        })
+  ])
+  
+      console.log(responses);
 
-    console.log(response);
+      }catch(error){
+        console.log(error);
+      }
+
+
     }
 
     fetchData()
